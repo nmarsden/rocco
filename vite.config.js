a69104411b17fe34df681b96c53412f91bea3f77
Neil Marsden <neil.marsden@oracle.com>
2025-02-01 15:34:17 +1100

    set vite base to /rocco/ for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { transformWithEsbuild } from 'vite'
 import restart from 'vite-plugin-restart'
 
 export default {
+    base: '/rocco/',
     root: 'src/',
     publicDir: '../public/',
     plugins:
