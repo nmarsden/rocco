db2b71e891d180e80dc77ddc4d2bcd4846695cc4
Neil Marsden <neil.marsden@oracle.com>
2025-02-02 13:43:01 +1100

    add light color tweaks

--- src/Lights.jsx
+++ src/Lights.jsx
@@ -1,18 +1,24 @@
 import {folder, useControls} from "leva";
 
 export default function Lights() {
-    const { ambientIntensity, directionalPosition, directionalIntensity } = useControls('Lights', {
+    const { ambientIntensity, ambientColor, directionalColor, directionalPosition, directionalIntensity } = useControls('Lights', {
         'Ambient': folder({
+            ambientColor: { value: '#ffffff', label: 'color' },
             ambientIntensity: { value: 1.5, label: 'intensity', min: 0, max: 10 }
         }),
         'Directional': folder({
+            directionalColor: { value: '#ffffff', label: 'color' },
             directionalPosition: { value: [3,3,3], label: 'position' },
             directionalIntensity: { value: 4.5, label: 'intensity', min: 0, max: 10 }
         })
     });
     return <>
-        <ambientLight intensity={ambientIntensity}/>
+        <ambientLight
+            color={ambientColor}
+            intensity={ambientIntensity}
+        />
         <directionalLight
+            color={directionalColor}
             position={directionalPosition}
             intensity={directionalIntensity}
             castShadow={true}
