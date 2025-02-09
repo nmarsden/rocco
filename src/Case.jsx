5193a3b2998cbc823b63a652b7e427e1d7b3655d
Neil Marsden <neil.marsden@oracle.com>
2025-02-09 17:23:27 +1100

    Add directional buttons

--- src/Case.jsx
+++ src/Case.jsx
@@ -2,9 +2,16 @@ import {useGLTF} from "@react-three/drei";
 import {folder, useControls} from "leva";
 import RoccoText from "./RoccoText.jsx";
 import React from "react";
+import * as THREE from "three";
+
+const buttonMaterial = new THREE.MeshStandardMaterial({
+    color: '#e2d9d9',
+    roughness: 0.0,
+    metalness: 0.45
+})
 
 export default function Case() {
-    const { nodes} = useGLTF('case-transformed.glb')
+    const { nodes} = useGLTF('case.glb')
 
     const { caseColor, buttonColor, roughness, metalness } = useControls(
         'World',
@@ -12,7 +19,6 @@ export default function Case() {
             'Case': folder(
                 {
                     caseColor: {value: '#be3b37', label: 'case color'},
-                    buttonColor: {value: '#e2d9d9', label: 'button color'},
                     roughness: {value: 0.0, label: 'roughness', min: 0, max: 10, step: 0.1},
                     metalness: {value: 0.45, label: 'metalness', min: 0, max: 10, step: 0.1},
                 },
@@ -40,20 +46,13 @@ export default function Case() {
                 />
                 <RoccoText/>
             </mesh>
-            <mesh
-                geometry={nodes.Button.geometry}
-                castShadow={true}
-                receiveShadow={true}
-            >
-                <meshStandardMaterial
-                    roughness={roughness}
-                    metalness={metalness}
-                    color={buttonColor}
-                />
-            </mesh>
-
+            <mesh castShadow={true} receiveShadow={true} geometry={nodes.Center_Button.geometry} material={buttonMaterial}/>
+            <mesh castShadow={true} receiveShadow={true} geometry={nodes.Down_Button.geometry} material={buttonMaterial}/>
+            <mesh castShadow={true} receiveShadow={true} geometry={nodes.Left_Button.geometry} material={buttonMaterial}/>
+            <mesh castShadow={true} receiveShadow={true} geometry={nodes.Right_Button.geometry} material={buttonMaterial}/>
+            <mesh castShadow={true} receiveShadow={true} geometry={nodes.Up_Button.geometry} material={buttonMaterial}/>
         </group>
     );
 }
 
-useGLTF.preload('case-transformed.glb')
+useGLTF.preload('case.glb')
