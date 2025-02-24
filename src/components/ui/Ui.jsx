a03e5a2ccfd88eb556d56f7d0a99b8b9d54d55bb
Neil Marsden <neil.marsden@oracle.com>
2025-02-25 08:13:06 +1100

    - add credits

--- src/components/ui/Ui.jsx
+++ src/components/ui/Ui.jsx
@@ -16,7 +16,16 @@ export default function Ui() {
                 <div className="heading">*********</div>
                 <div className="tagLine">The digital pet rock</div>
                 <div className="author">by Neil Marsden</div>
-                <div className="links"><a href="https://github.com/nmarsden/rocco" target="_blank">github</a> | <a href="https://nmarsden.com" target="_blank">projects</a></div>
+                <div className="links">
+                    <a href="https://github.com/nmarsden/rocco" target="_blank">github</a>|
+                    <a href="https://nmarsden.com" target="_blank">projects</a>
+                </div>
+                <div className="sub-heading">-= credits =-</div>
+                <div className="links">
+                    <a href="https://dustyroom.com/free-casual-game-sounds" target="_blank">sound</a>|
+                    <a href="https://fonts.google.com/specimen/Doto" target="_blank">font</a>|
+                    <a href="https://drububu.com/miscellaneous/voxelizer" target="_blank">voxelizer</a>
+                </div>
             </div>
         </>
     )
